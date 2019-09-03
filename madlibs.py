7ccdbc23ddc1c8c41895eb0d2264a91064c663df
Louis Dweck <louisdweck@Louiss-MacBook-Pro.local>
2019-09-03 09:44:49 -0700

    learned something new

--- madlibs.py
+++ madlibs.py
@@ -8,5 +8,5 @@ for input_type in input_types:
 
 # print(user_inputs)
 
-story = 'One day three ' + user_inputs[0] + ' saw a group of ' + user_inputs[1] + user_inputs[2] + ' and frolicking ' + user_input[3] + 'through the woods to house of ' + user_input[4] + 'But on their way through the forest, they were attacked by a group of viscous baboons ' + user_input[5] + '. ' + ' Thankfully evryone was unscathed and ventured back to the zoo. '
+story = 'One day three ' + user_inputs[0] + ' saw a group of ' + user_inputs[1] + ' ' + user_inputs[2] + ' and frolicking ' + user_inputs[3] + ' through the woods to the house of ' + user_inputs[4] + '. But on their way through the forest, they were attacked by a group of viscous baboons ' + user_inputs[5] + '. ' + ' Thankfully everyone was unscathed and ventured back to the zoo. '
 print(story)
