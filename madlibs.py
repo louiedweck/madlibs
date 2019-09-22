731af7df6b38524e700e330059c163be7e5805ed
louiedweck <dweck.louis@gmail.com>
2019-09-21 23:39:27 -0700

    new

--- madlibs.py
+++ madlibs.py
@@ -12,5 +12,5 @@ for input_type in input_types:
 story = 'One day three ' + user_inputs[0] + ' saw a group of ' + user_inputs[1] + ' ' + user_inputs[2] + ' and frolicking ' + user_inputs[3] + ' through the woods to the house of ' + user_inputs[4] + \
     '. But on their way through the forest, they were attacked by a group of viscous baboons ' + \
         user_inputs[5] + '. ' + \
-    ' Thankfully everyone was unscathed and ventured back home. If only they knew what they had in store next.'
+    ' Thankfully everyone was unscathed and ventured back home. If only they knew what they had in store next. '
 print(story)
