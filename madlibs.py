7f974d2e927eb08fd748a9deb84a1af12a36eb30
louiedweck <dweck.louis@gmail.com>
2019-09-21 23:37:49 -0700

    updated story

--- madlibs.py
+++ madlibs.py
@@ -1,4 +1,5 @@
-input_types = ['plural animal', 'different plural animal','verb', 'adverb', 'name', 'verb']
+input_types = ['plural animal', 'different plural animal',
+               'verb', 'adverb', 'name', 'verb']
 user_inputs = []
 
 for input_type in input_types:
@@ -8,5 +9,8 @@ for input_type in input_types:
 
 # print(user_inputs)
 
-story = 'One day three ' + user_inputs[0] + ' saw a group of ' + user_inputs[1] + ' ' + user_inputs[2] + ' and frolicking ' + user_inputs[3] + ' through the woods to the house of ' + user_inputs[4] + '. But on their way through the forest, they were attacked by a group of viscous baboons ' + user_inputs[5] + '. ' + ' Thankfully everyone was unscathed and ventured back home. '
+story = 'One day three ' + user_inputs[0] + ' saw a group of ' + user_inputs[1] + ' ' + user_inputs[2] + ' and frolicking ' + user_inputs[3] + ' through the woods to the house of ' + user_inputs[4] + \
+    '. But on their way through the forest, they were attacked by a group of viscous baboons ' + \
+        user_inputs[5] + '. ' + \
+    ' Thankfully everyone was unscathed and ventured back home. If only they knew what they had in store next.'
 print(story)
